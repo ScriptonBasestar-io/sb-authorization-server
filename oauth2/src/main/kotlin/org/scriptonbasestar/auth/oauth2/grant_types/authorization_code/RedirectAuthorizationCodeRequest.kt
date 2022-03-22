26284305afcbb5967eb193681a6b0277624e2e67
CEE <archmagece@gmail.com>
2022-03-23 01:49:45 +0900

    remove unused

--- oauth2/src/main/kotlin/org/scriptonbasestar/auth/oauth2/grant_types/authorization_code/RedirectAuthorizationCodeRequest.kt
+++
@@ -1,9 +0,0 @@
-package org.scriptonbasestar.auth.oauth2.grant_types.authorization_code
-
-class RedirectAuthorizationCodeRequest(
-    val clientId: String?,
-    val redirectUri: String?,
-    val username: String?,
-    val password: String?,
-    val scope: String?
-)
--- oauth2/src/main/kotlin/org/scriptonbasestar/auth/oauth2/grant_types/authorization_code/RedirectTokenRequest.kt
+++
@@ -1,9 +0,0 @@
-package org.scriptonbasestar.auth.oauth2.grant_types.authorization_code
-
-class RedirectTokenRequest(
-    val clientId: String?,
-    val redirectUri: String?,
-    val username: String?,
-    val password: String?,
-    val scope: String?
-)
